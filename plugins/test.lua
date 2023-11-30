864598ef04ca3679fadc40494bbf217a4cd3d30f
Erison Silva <hey@erison.work>
2023-11-30 18:12:50 +0100

    Fixing neotest

--- plugins/test.lua
+++ plugins/test.lua
@@ -1,4 +1,6 @@
 -- It is WIP, there is  issue on gitbug: https://github.com/olimorris/neotest-phpunit/pull/18
+local prefix = "<leader>t"
+
 return {
   "nvim-neotest/neotest",
   -- lazy = true,
@@ -12,17 +14,18 @@ return {
       running = {
         concurrent = true,
       },
+      status = {
+        enabled = false,
+        signs = false,
+        virtual_text = false,
+      },
       adapters = {
         args = { "--log-level", "DEBUG" },
         require "neotest-phpunit" {
           phpunit_cmd = function()
             return {
-              "docker-compose",
-              "-f",
-              "docker/docker-compose.yml",
-              "exec",
-              "app",
-              "bin/phpunit",
+              "sh",
+              "nvim/neotest.sh", -- docker compose -f docker/docker-compose.yml exec app bin/phpunit $1
             }
           end,
 
@@ -35,4 +38,18 @@ return {
       },
     }
   end,
+  -- https://www.lazyvim.org/extras/test/core#neotest-1
+  keys = {
+    { prefix .. "t", function() require("neotest").run.run(vim.fn.expand "%") end, desc = "Run File" },
+    { prefix .. "T", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
+    { prefix .. "r", function() require("neotest").run.run() end, desc = "Run Nearest" },
+    { prefix .. "s", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+    {
+      prefix .. "o",
+      function() require("neotest").output.open { enter = true, auto_close = true } end,
+      desc = "Show Output",
+    },
+    { prefix .. "O", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
+    { prefix .. "S", function() require("neotest").run.stop() end, desc = "Stop" },
+  },
 }
